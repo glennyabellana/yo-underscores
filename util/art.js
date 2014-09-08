eb387374728931dc4b27be4825ec7b84d0240289
fainder <chee@fainder.info>
2014-09-08 13:47:08 +0900

    Update chalk art

--- util/art.js
+++ util/art.js
@@ -2,13 +2,14 @@ var chalk = require('chalk');
 
 module.exports = {
 	emi : [
-		'',
-		'███████╗ ███╗   ███╗ ██╗',
-		'██╔════╝ ████╗ ████║ ██║',
-		'█████╗   ██╔████╔██║ ██║',
-		'██╔══╝   ██║╚██╔╝██║ ██║',
-		'███████╗ ██║ ╚═╝ ██║ ██║',
-		'╚══════╝ ╚═╝     ╚═╝ ╚═╝',
-		''
+		'\n     _-----_' +
+		'\n    |       |' +
+		'\n    |' + chalk.red('--(o)--') + '|   .--------------------------.' +
+		'\n   `---------´  |    ' + chalk.yellow.bold('Welcome to generator-underscores') + ',    |' +
+		'\n    ' + chalk.yellow('(') + ' _' + chalk.yellow('´U`') + '_ ' + chalk.yellow(')') + '   |   ' + chalk.yellow.bold('ladies and gentlemen!') + '  |' +
+		'\n    /___A___\\   \'__________________________\'' +
+		'\n     ' + chalk.yellow('|  ~  |') +
+		'\n   __' + chalk.yellow('\'.___.\'') + '__' +
+		'\n ´   ' + chalk.red('`  |') + '° ' + chalk.red('´ Y') + ' `\n'
 	].join('\n')
 };
