d45a1a175977a236e8bf691d77f7d0a1a7d0d4bf
fainder <chee@fainder.info>
2014-09-08 14:04:00 +0900

    Fix ascii art

--- util/art.js
+++ util/art.js
@@ -4,10 +4,10 @@ module.exports = {
 	emi : [
 		'\n     _-----_' +
 		'\n    |       |' +
-		'\n    |' + chalk.red('--(o)--') + '|   .--------------------------.' +
-		'\n   `---------´  |    ' + chalk.yellow.bold('Welcome to generator-underscores') + ',    |' +
-		'\n    ' + chalk.yellow('(') + ' _' + chalk.yellow('´U`') + '_ ' + chalk.yellow(')') + '   |   ' + chalk.yellow.bold('ladies and gentlemen!') + '  |' +
-		'\n    /___A___\\   \'__________________________\'' +
+		'\n    |' + chalk.red('--(o)--') + '|   .----------------------------------------.' +
+		'\n   `---------´  |    ' + chalk.yellow.bold('Welcome to generator-underscores') + ',   |' +
+		'\n    ' + chalk.yellow('(') + ' _' + chalk.yellow('´U`') + '_ ' + chalk.yellow(')') + '   |    ' + chalk.yellow.bold('ladies and gentlemen!') + '               |' +
+		'\n    /___A___\\   \'________________________________________\'' +
 		'\n     ' + chalk.yellow('|  ~  |') +
 		'\n   __' + chalk.yellow('\'.___.\'') + '__' +
 		'\n ´   ' + chalk.red('`  |') + '° ' + chalk.red('´ Y') + ' `\n'
